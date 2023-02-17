50f0ecfff838d4c344d6da7a0b77827d6fd26b67
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-17 20:43:55 +0100

    correction to select filter genres

--- api.php
+++ api.php
@@ -1,63 +1,60 @@
 <?php
 
 //! SQL FILE
-// // Preparo le informazioni di connessione
-// const DB_SERVERNAME = 'localhost';
-// const DB_USERNAME = 'root';
-// const DB_PASSWORD = 'root';
-// const DB_NAME = 'discs';
+// Preparo le informazioni di connessione
+const DB_SERVERNAME = 'localhost';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = 'root';
+const DB_NAME = 'discs';
 
-// try {
-//     $conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
-// } catch (Exception $e) {
-//     echo $e->getMessage();
-//     die();
-// }
+$discs = [];
 
-// try {
-//     $sql = 'SELECT * FROM `albums`';
-//     $result = $conn->query($sql);
+try {
+    $conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+}
 
-//     if ($result->num_rows) {
-//         while ($row = $result->fetch_assoc()) {
-//             echo $row['title'];
-//         }
-//     } else {
-//         echo 'Spiacente';
-//     }
-// } catch (Exception $e) {
-//     echo $e->getMessage();
-//     die();
-// }
+try {
+    $sql = 'SELECT * FROM `albums`';
+    $result = $conn->query($sql);
 
-// $conn->close();
+    if ($result->num_rows) {
+        while ($row = $result->fetch_assoc()) {
+            array_push($discs, $row);
+        }
+    } else {
+        echo 'Spiacente';
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+}
+
+$conn->close();
 
 
 //! JSON FILE
 
-// Recover json file 
-$source_path = __DIR__ . './data.json';
+// // Recover json file 
+// $source_path = __DIR__ . './data.json';
 
-// Read content file
-$json_data = file_get_contents($source_path);
+// // Read content file
+// $json_data = file_get_contents($source_path);
 
 
 // Take choice user in select for genre filter
 $genre = $_GET['genre'] ?? '';
-if ($genre) {
-    // Convert file json to php
-    $discs = json_decode($json_data, true);
-
-    $filtered_discs = [];
-    foreach ($discs as $disc) {
-        if ($disc['genre'] === $genre) $filtered_discs[] = $disc;
+$filtered_discs = [];
+foreach ($discs as $disc) {
+    if ($genre === '' || $disc['genre'] === $genre) {
+        $filtered_discs[] = $disc;
     }
-
-    $json_data = json_encode($filtered_discs);
 }
 
-// Tell to server I want to write in json
+//  Tell to server I want to write in json
 header('Content-Type: application/json');
 
-// Convert to json and return discs
-echo $json_data;
+//  Convert to json and return discs
+echo json_encode($filtered_discs);
